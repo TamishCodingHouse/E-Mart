840480b96c5862677d73e8cd26267647049317b4
squadminds <vishaldeep0106@gmail.com>
2025-09-19 18:31:12 +0530

    Today's work: added product filter and UI changes

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Banner from "./components/Banner"
+// import { Routes, Route } from 'react-router-dom'
+import Cat from './components/Cat'
+<<<<<<< HEAD
+import Product from './components/Product'
+=======
+>>>>>>> 1673417fe3716c5074acf6d473bcc1a490dd762d
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Banner/>
+      <Cat/>
+<<<<<<< HEAD
+      <Product/>
+=======
+>>>>>>> 1673417fe3716c5074acf6d473bcc1a490dd762d
+    </div>
+  )
+}
+
+export default App
